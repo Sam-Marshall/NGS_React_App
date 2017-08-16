4596a80bfd7742a1eb596fdf5a9e1b439f6568c6
Greg Crawford <gpcrawford@gmail.com>
2017-08-16 16:24:37 +0000

    Load DB user, password from .env

--- models/index.js
+++ models/index.js
@@ -7,12 +7,15 @@ var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../app/config/config.json')[env];
 var db        = {};
-
+/*
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
+*/
+
+var sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, config);
 
 fs
   .readdirSync(__dirname)
